bd5ce28d5bac433c443b720be610332a1aa1502a
Lionel TOTON <totonlionel@gmail.com>
2024-10-24 23:15:54 +0200

    lionel

--- Cours/4-Outils_de_résolution_de_problèmes_complexes_RO_et_IAs/Code_Machine_learning/K-near-neighbors/knn-case.py
+++ Cours/4-Outils_de_résolution_de_problèmes_complexes_RO_et_IAs/Code_Machine_learning/K-near-neighbors/knn-case.py
@@ -1,18 +1,20 @@
 from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import _pprint
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 
 # Chargement d'un jeu de données
 iris = load_iris()
+
 X = iris.data
 y = iris.target
 
 # Séparation des données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=42)
 
 # Création d'un modèle KNN avec k=3
-knn = KNeighborsClassifier(n_neighbors=3)
+knn = KNeighborsClassifier(n_neighbors=6)
 
 # Entraînement du modèle
 knn.fit(X_train, y_train)
